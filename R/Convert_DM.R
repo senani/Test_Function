e52bf5f5e699e5cb7076f5983d1daad063300e49
Senani Karunaratne <vic6shz@lcm.dpi.vic.gov.au>
2019-11-19 11:38:56 +1100

    Senani first

---
+++ R/Convert_DM.R
@@ -0,0 +1,19 @@
+
+#' Simple R Function for converting DM to Percent
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dm <- function(x) {
+  dry_p <- x + 5
+  dry_final <- dry_p * 100
+  x <- plot(x, dry_final)
+  return(dry_final)
+
+}
+
+
+
